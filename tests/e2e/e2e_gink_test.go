d203d3fa1a24e6dbbff0debe4400ac64f28bfec5
Eduard Serra <eduser25@gmail.com>
2020-09-24 10:44:51 -0700

    More stuff

--- tests/e2e/e2e_gink_test.go
+++ tests/e2e/e2e_gink_test.go
@@ -10,33 +10,97 @@ var _ = Describe("Test and deploy a simple mesh", func() {
 	var td OsmTestData
 
 	Context("Test OSM testing APIs", func() {
-		var ns []string = []string{"sender", "receiver"}
+		sourceNs := "client"
+		destNs := "server"
+		var ns []string = []string{sourceNs, destNs}
 
-		It("Initializes Test flags, data, and cluster configuration", func() {
+		It("Test testing APIs in a simple e2e test", func() {
+			// Init test
 			td = InitTestData(GinkgoT())
-		})
 
-		// It("Updates images on container registry", func() {
-		// 	// Do we want this on test basis?
-		// })
+			// For cleanup only while testing, not needed
+			for _, n := range ns {
+				td.Namespaces[n] = true
+			}
+			td.Namespaces[td.osmMeshName] = true
 
-		It("Installs OSM", func() {
+			// Install OSM
 			td.InstallOSM(td.GetTestInstallOpts())
-		})
+			td.WaitForPodsRunningReady(td.osmMeshName, 30*time.Second)
 
-		It("Create test namespaces", func() {
+			// Create Test NS
 			for _, n := range ns {
 				td.CreateNs(n, nil)
 			}
-		})
 
-		// More steps coming
+			// Add Namespaces to mesh
+			td.AddNsToMesh(ns...)
 
-		It("Deletes Namespace deletion upon test finish", func() {
-			for _, n := range ns {
-				td.DeleteNs(n)
+			// Get simple pod definitions for the HTTP server
+			svcAccDef, podDef, svcDef := td.SimplePodApp(
+				SimplePodAppDef{
+					name:      "server",
+					namespace: destNs,
+					image:     "kennethreitz/httpbin",
+				})
+
+			td.CreateServiceAccount(destNs, &svcAccDef)
+			dstPod, _ := td.CreatePod(destNs, podDef)
+			td.CreateService(destNs, svcDef)
+
+			// Expect it to be up and running in it's receiver namespace
+			td.WaitForPodsRunningReady(destNs, 30*time.Second)
+
+			// Get simple Pod definitions for the client
+			svcAccDef, podDef, svcDef = td.SimplePodApp(SimplePodAppDef{
+				name:      "client",
+				namespace: sourceNs,
+				command:   []string{"/bin/bash", "-c", "--"},
+				args:      []string{"while true; do sleep 30; done;"},
+				image:     "songrgg/alpine-debug",
+			})
+
+			td.CreateServiceAccount(sourceNs, &svcAccDef)
+			srcPod, _ := td.CreatePod(sourceNs, podDef)
+			td.CreateService(sourceNs, svcDef)
+
+			// Expect it to be up and running in it's receiver namespace
+			td.WaitForPodsRunningReady(sourceNs, 30*time.Second)
+
+			// Deploy allow rule client->server
+			httpRG, trafficTarget := td.CreateSimpleAllowPolicy(
+				SimpleAllowPolicy{
+					RouteGroupName:    "routes",
+					TrafficTargetName: "test-target",
+
+					SourceNamespace:      sourceNs,
+					SourceSVCAccountName: "client",
+
+					DestinationNamespace:      destNs,
+					DestinationSvcAccountName: "server",
+				})
+
+			td.CreateHTTPRouteGroup(httpRG)
+			td.CreateTrafficTarget(trafficTarget)
+
+			// All ready. Expect client to reach server
+			// Need to get the pod though.
+
+			for true {
+				td.HTTPRequest(HTTPRequestTest{
+					sourceNs:        srcPod.Namespace,
+					sourcePod:       srcPod.Name,
+					sourceContainer: "client", // We can do better
+
+					destHostname: "server",
+					destNs:       dstPod.Namespace,
+
+					httpUrl: "/",
+					port:    80,
+				})
+				time.Sleep(2 * time.Second)
 			}
-			td.WaitForNamespacesDeleted(ns, 30*time.Second)
+
 		})
 	})
 
