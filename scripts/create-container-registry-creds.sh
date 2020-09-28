8dc2b8fb0dcfa330aecceb37ca0b98904c22f121
Eduard Serra <eduser25@gmail.com>
2020-09-28 13:08:23 -0700

    Fix unrightful addition of commit

--- scripts/create-container-registry-creds.sh
+++ scripts/create-container-registry-creds.sh
@@ -3,7 +3,6 @@
 set -aueo pipefail
 
 # shellcheck disable=SC1091
-source ../../.env
 
 [[ $# -eq 0 ]] && { echo "Missing required namespace argument"; exit 1; }
 namespace="$1"
@@ -17,7 +16,6 @@ fi
 
 REGISTRY=$(echo "$CTR_REGISTRY" | awk -F'.' '{print $1}')
 REGISTRY_URL=$(echo "$CTR_REGISTRY" | awk -F'.' '{print $1 "." $2 "." $3}')
-CTR_REGISTRY_CREDS_NAME="${CTR_REGISTRY_CREDS_NAME:-acr-creds}"
 
 echo "Creating container registry credentials ($CTR_REGISTRY_CREDS_NAME) for Kubernetes in namespace ($namespace) for the given Azure Container Registry ($REGISTRY_URL)"
 
