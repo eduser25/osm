e1bc2a14c8f65e1a8fb9d20912c8e6dede19e534
Eduard Serra <eduser25@gmail.com>
2020-10-01 13:22:16 -0700

    Wrapping up second test
    - Includes some helpers to create Async HTTP request
    
    We might add a more generic abstraction for further tests which
    encompasses the HTTP request testing from multiple sources to one
    destination. Seems a useful abstraction that might very well be
    needed going forward.

--- tests/e2e/e2e_10svc_100pods_test.go
+++ tests/e2e/e2e_10svc_100pods_test.go
@@ -1,29 +1,43 @@
 package e2e
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"github.com/ahmetalpbalkan/go-cursor"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Test 10(x10 pods) Clients -> 1(x10 pods) server", func() {
 	Context("DeploymentsClientServer", func() {
 		destApp := "server"
 		sourceAppBaseName := "client"
-		sourceApplications := []string{}
+		var sourceNamespaces []string = []string{} // for this test, (Service name == NS name == container name)
+		var podsPerNamespace map[string][]string = map[string][]string{}
 
-		// Total 100 pods
-		numberOfClientApps := 10
+		// Total (numberOfClientApps x replicaSetPerApp) pods
+		numberOfClientApps := 5
 		replicaSetPerApp := 10
 
+		// Used accross the test to wait for concurrent steps to finish
+		var wg sync.WaitGroup
+
 		for i := 0; i < numberOfClientApps; i++ {
-			sourceApplications = append(sourceApplications, fmt.Sprintf("%s%d", sourceAppBaseName, i))
+			sourceNamespaces = append(sourceNamespaces, fmt.Sprintf("%s%d", sourceAppBaseName, i))
 		}
 
 		It("Tests HTTP traffic for a simple client-server pod deployment", func() {
+			// For Cleanup only
+			td.Namespaces["server"] = true
+			for _, srcClient := range sourceNamespaces {
+				td.Namespaces[srcClient] = true
+			}
+
 			// Install OSM
 			Expect(td.InstallOSM(td.GetTestInstallOpts())).To(Succeed())
 			Expect(td.WaitForPodsRunningReady(td.osmMeshName, 60*time.Second, 1)).To(Succeed())
@@ -34,13 +48,10 @@ var _ = Describe("Test 10(x10 pods) Clients -> 1(x10 pods) server", func() {
 			Expect(td.AddNsToMesh(true, destApp)).To(Succeed())
 
 			// Client Applications
-			for _, srcClient := range sourceApplications {
+			for _, srcClient := range sourceNamespaces {
 				Expect(td.CreateNs(srcClient, nil)).To(Succeed())
 				Expect(td.AddNsToMesh(true, srcClient)).To(Succeed())
 			}
-
-			// To wait for all
-			var wg sync.WaitGroup
 
 			// Get simple pod definitions for the HTTP server
 			svcAccDef, deploymentDef, svcDef := td.SimpleDeploymentApp(
@@ -64,7 +75,7 @@ var _ = Describe("Test 10(x10 pods) Clients -> 1(x10 pods) server", func() {
 				Expect(td.WaitForPodsRunningReady(destApp, 200*time.Second, replicaSetPerApp)).To(Succeed())
 			}(&wg, destApp)
 
-			for _, srcClient := range sourceApplications {
+			for _, srcClient := range sourceNamespaces {
 				svcAccDef, deploymentDef, svcDef = td.SimpleDeploymentApp(
 					SimpleDeploymentAppDef{
 						name:         srcClient,
@@ -87,8 +98,118 @@ var _ = Describe("Test 10(x10 pods) Clients -> 1(x10 pods) server", func() {
 					Expect(td.WaitForPodsRunningReady(srcClient, 200*time.Second, replicaSetPerApp)).To(Succeed())
 				}(&wg, srcClient)
 			}
-
 			wg.Wait()
+
+			// Create traffic rules
+			// Deploy allow rules (10x)clients -> server
+			for _, srcClient := range sourceNamespaces {
+				httpRG, trafficTarget := td.CreateSimpleAllowPolicy(
+					SimpleAllowPolicy{
+						RouteGroupName:    srcClient,
+						TrafficTargetName: srcClient,
+
+						SourceNamespace:      srcClient,
+						SourceSVCAccountName: srcClient,
+
+						DestinationNamespace:      destApp,
+						DestinationSvcAccountName: destApp,
+					})
+
+				// Configs have to be put into a monitored NS, and osm-system can't be by cli
+				_, err = td.CreateHTTPRouteGroup(srcClient, httpRG)
+				Expect(err).NotTo(HaveOccurred())
+				_, err = td.CreateTrafficTarget(srcClient, trafficTarget)
+				Expect(err).NotTo(HaveOccurred())
+			}
+
+			// Get Deterministic list of pods per source application
+			// Walking maps is NOT deterministic in golang, this causes scrambled output later when walking map ranges repeatedly
+			for _, ns := range sourceNamespaces {
+				// Get pods for every service (assuming every service is enclosed in a namespace, we can get all pods for an NS)
+				pods, err := td.Client.CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{})
+				Expect(err).To(BeNil())
+
+				for _, pod := range pods.Items {
+					_, found := podsPerNamespace[ns]
+					if !found {
+						podsPerNamespace[ns] = []string{}
+					}
+					podsPerNamespace[ns] = append(podsPerNamespace[ns], pod.Name)
+				}
+			}
+
+			// Map[ns][pod] -> HTTPResults
+			var results map[string]map[string]*HTTPAsyncResults = make(map[string]map[string]*HTTPAsyncResults)
+			// synchronization artifacts. We will reuse the global waitgroup
+			var stop bool = false
+
+			// Start HTTP async threads
+			for _, ns := range sourceNamespaces {
+				for _, pod := range podsPerNamespace[ns] {
+					// Create a Result
+					_, ok := results[ns]
+					if !ok {
+						results[ns] = make(map[string]*HTTPAsyncResults)
+					}
+					results[ns][pod] = NewHTTPAsyncResults()
+
+					go td.AsynchronousHTTPRunner(
+						HTTPAsync{
+							RequestData: HTTPRequestDef{
+								SourceNs:        ns,
+								SourcePod:       pod,
+								SourceContainer: ns, // container_name == NS for this test
+
+								Destination: fmt.Sprintf("%s.%s", destApp, destApp),
+
+								HTTPUrl: "/",
+								Port:    80,
+							},
+							ResponseDataStore: results[ns][pod],
+							StopSignal:        &stop,
+							WaitGroup:         &wg,
+							SleepTime:         1 * time.Second,
+						},
+					)
+				}
+			}
+
+			// Inspect results and wait for success/failure
+			success := WaitForRepeatedSuccess(func() bool {
+				var overallSuccess bool = true
+
+				for _, ns := range sourceNamespaces {
+					podNames := []string{}
+					podResultsToPrint := []*HTTPAsyncResults{}
+
+					for _, pod := range podsPerNamespace[ns] {
+						podRes := results[ns][pod]
+
+						podResultsToPrint = append(podResultsToPrint, podRes)
+						podNames = append(podNames, pod)
+
+						podRes.withLock(func() {
+							if podRes.lastResult.Err != nil || podRes.lastResult.StatusCode != 200 {
+								overallSuccess = false
+							}
+						})
+					}
+					td.PrettyPrintHTTPResults(ns, podNames, podResultsToPrint)
+				}
+				// Meaningful only on shell
+				td.T.Log(cursor.MoveUp(len(sourceNamespaces) + 1))
+
+				return overallSuccess
+			}, 5, 150*time.Second)
+
+			// Meaningful only on shell
+			td.T.Log(cursor.MoveDown(len(sourceNamespaces) + 1))
+
+			// Stop all async threads
+			stop = true
+			wg.Wait()
+
+			Expect(success).To(BeTrue())
 		})
 	})
 })
--- tests/e2e/e2e_pod_client_server_test.go
+++ tests/e2e/e2e_pod_client_server_test.go
@@ -83,7 +83,7 @@ var _ = Describe("Simple Client-Server pod test", func() {
 			// All ready. Expect client to reach server
 			// Need to get the pod though.
 			cond := WaitForRepeatedSuccess(func() bool {
-				statusCode, _, err :=
+				result :=
 					td.HTTPRequest(HTTPRequestDef{
 						SourceNs:        srcPod.Namespace,
 						SourcePod:       srcPod.Name,
@@ -95,11 +95,11 @@ var _ = Describe("Simple Client-Server pod test", func() {
 						Port:    80,
 					})
 
-				if err != nil || statusCode != 200 {
-					td.T.Logf("> REST req failed (status: %d) %v", statusCode, err)
+				if result.Err != nil || result.StatusCode != 200 {
+					td.T.Logf("> REST req failed (status: %d) %v", result.StatusCode, result.Err)
 					return false
 				}
-				td.T.Logf("> REST req succeeded: %d", statusCode)
+				td.T.Logf("> REST req succeeded: %d", result.StatusCode)
 				return true
 			}, 5, 60*time.Second)
 			Expect(cond).To(BeTrue())
