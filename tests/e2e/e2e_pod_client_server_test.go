18650f3cf6df850407644be7e4744f8e4b03cf3f
Eduard Serra <eduser25@gmail.com>
2020-10-01 13:22:16 -0700

    Wrapping up second test
    - Includes some helpers to create Async HTTP request
    
    We might add a more generic abstraction for further tests which
    encompasses the HTTP request testing from multiple sources to one
    destination. Seems a useful abstraction that might very well be
    needed going forward.

--- tests/e2e/e2e_pod_client_server_test.go
+++ tests/e2e/e2e_pod_client_server_test.go
@@ -84,7 +84,7 @@ var _ = Describe("Simple Client-Server pod test", func() {
 			// All ready. Expect client to reach server
 			// Need to get the pod though.
 			cond := WaitForRepeatedSuccess(func() bool {
-				statusCode, _, err :=
+				result :=
 					td.HTTPRequest(HTTPRequestDef{
 						SourceNs:        srcPod.Namespace,
 						SourcePod:       srcPod.Name,
@@ -96,11 +96,11 @@ var _ = Describe("Simple Client-Server pod test", func() {
 						Port:    80,
 					})
 
-				if err != nil || statusCode != 200 {
-					td.T.Logf("> REST req failed (status: %d) %v", statusCode, err)
+				if result.Err != nil || result.StatusCode != 200 {
+					td.T.Logf("> REST req failed (status: %d) %v", result.StatusCode, result.Err)
 					return false
 				}
-				td.T.Logf("> REST req succeeded: %d", statusCode)
+				td.T.Logf("> REST req succeeded: %d", result.StatusCode)
 				return true
 			}, 5, 60*time.Second)
 			Expect(cond).To(BeTrue())
